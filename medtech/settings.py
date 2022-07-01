f4f15f5b2974c1c83bcc2a1f03bbed0d00e80f81
Fattijenishbek <fattijenishbek01@gmail.com>
2022-07-01 22:37:43 +0600

    db settings changed

--- medtech/settings.py
+++ medtech/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'medtech.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'PORT': config('DB_PORT'),
-#         'HOST': config('DB_HOST'),
-#     }
-# }
-
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.sqlite3',
-'NAME': BASE_DIR/'db.sqlite3',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'PORT': config('DB_PORT'),
+        'HOST': config('DB_HOST'),
+    }
 }
-}
+
+# DATABASES = {
+# 'default': {
+# 'ENGINE': 'django.db.backends.sqlite3',
+# 'NAME': BASE_DIR/'db.sqlite3',
+# }
+# }
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
