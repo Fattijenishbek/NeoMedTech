13dbc9246688770b5c73ba1f0ff1e4bb390513f9
Agahan14 <Imamidinov.agahan06@gmail.com>
2022-07-11 21:36:24 +0600

    adding schedule logic

--- schedule/serializers.py
+++ schedule/serializers.py
@@ -48,18 +48,19 @@ class AppointmentSerializer(serializers.ModelSerializer):
         date = data.get('date')
         start_time = data.get('start_time')
         end_time = data.get('end_time')
-        doctor = date.get('doctor')
+        doc = data.get('doctor')
         inn = data.get('inn')
 
         if not Patient.objects.get(inn=inn):
             raise serializers.ValidationError("The inn doesn't match!")
-
-        if not WorkDate.objects.filter(date=date).filter(start__lte=start_time).filter(
-                end__gte=end_time).exists() and not Schedule.objects.filter(pk=doctor):
+        
+        if not Schedule.objects.filter(doctor_id=doc.id, work_date__date=date, work_date__start__lte=start_time,
+                                       work_date__end__gte=end_time).exists():
             raise serializers.ValidationError('The doctor is not working at this time!')
-        if Appointment.objects.filter(Q(start_time__range=[start_time, end_time]), date=date).exists():
+        if Appointment.objects.filter(Q(start_time__range=[start_time, end_time]), date=date,
+                                      doctor_id=doc.id).exists():
             raise serializers.ValidationError('This time is already booked!')
-        elif Appointment.objects.filter(Q(end_time__range=[start_time, end_time]), date=date).exists():
+        elif Appointment.objects.filter(Q(end_time__range=[start_time, end_time]), date=date,
+                                        doctor_id=doc.id).exists():
             raise serializers.ValidationError('This time is already booked!')
-
         return data
