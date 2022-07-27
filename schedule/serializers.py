1353d2c66fc2ce82a933220d666a07de3cfdc561
estebesov01 <sumseasonl1t1@gmail.com>
2022-07-28 02:57:21 +0600

    create serializers

--- schedule/serializers.py
+++ schedule/serializers.py
@@ -1,11 +1,30 @@
+import datetime
+
 from rest_framework import serializers
 
 from .models import (
     Schedule,
     Appointment,
     TimeSlots,
-    WorkDays,
+    Holidays
 )
+
+
+class TimeSlotsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TimeSlots
+        fields = '__all__'
+
+
+class TimeSlotsListSerializer(serializers.ModelSerializer):
+    times = serializers.SerializerMethodField()
+
+    class Meta:
+        model = TimeSlots
+        fields = ['id', 'times']
+
+    def get_times(self, obj):
+        return f'{obj.start} - {obj.end}'
 
 
 class ScheduleSerializer(serializers.ModelSerializer):
@@ -15,8 +34,25 @@ class ScheduleSerializer(serializers.ModelSerializer):
 
 
 class ScheduleListSerializer(ScheduleSerializer):
-    work_days = serializers.StringRelatedField(many=True)
     doctor = serializers.StringRelatedField()
+    monday = serializers.StringRelatedField(many=True)
+    tuesday = serializers.StringRelatedField(many=True)
+    wednesday = serializers.StringRelatedField(many=True)
+    thursday = serializers.StringRelatedField(many=True)
+    friday = serializers.StringRelatedField(many=True)
+    saturday = serializers.StringRelatedField(many=True)
+    sunday = serializers.StringRelatedField(many=True)
+
+
+class ScheduleForBookingSerializer(ScheduleSerializer):
+    doctor = serializers.StringRelatedField()
+    monday = TimeSlotsSerializer(many=True)
+    tuesday = TimeSlotsSerializer(many=True)
+    wednesday = TimeSlotsSerializer(many=True)
+    thursday = TimeSlotsSerializer(many=True)
+    friday = TimeSlotsSerializer(many=True)
+    saturday = TimeSlotsSerializer(many=True)
+    sunday = TimeSlotsSerializer(many=True)
 
 
 class AppointmentSerializer(serializers.ModelSerializer):
@@ -25,23 +61,32 @@ class AppointmentSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class AppointmentForBookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Appointment
+        fields = ['doctor', 'patient', 'date']
+
+    def validate_date(self, value):
+        if value < datetime.date.today():
+            raise serializers.ValidationError('This day is already in the past')
+        return value
+
+
 class AppointmentListSerializer(AppointmentSerializer):
     doctor = serializers.StringRelatedField()
     patient = serializers.StringRelatedField()
     time_slots = serializers.StringRelatedField()
 
 
-class TimeSlotsSerializer(serializers.ModelSerializer):
+class AppointmentGetTimesSerializer(serializers.ModelSerializer):
+    time_slots = TimeSlotsListSerializer()
+
     class Meta:
-        model = TimeSlots
-        fields = '__all__'
+        model = Appointment
+        fields = ['time_slots']
 
 
-class WorkDaysSerializer(serializers.ModelSerializer):
+class HolidaySerializer(serializers.ModelSerializer):
     class Meta:
-        model = WorkDays
+        model = Holidays
         fields = '__all__'
-
-
-class WorkDaysListSerializer(WorkDaysSerializer):
-    time_slots = TimeSlotsSerializer()
