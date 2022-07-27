dc6de244916b2898de5a692365b17a17ec5faa1e
estebesov01 <sumseasonl1t1@gmail.com>
2022-07-28 03:05:14 +0600

    add validate for phone field if it not numeric

--- users/services.py
+++ users/services.py
@@ -12,10 +12,10 @@ class ModelSerializerWithValidate(serializers.ModelSerializer):
                 (today.month, today.day) < (obj.birth_date.month, obj.birth_date.day))
 
     def validate_phone(self, value):
+        if not value[1:].isnumeric():
+            raise serializers.ValidationError('Phone must be numeric symbols')
         if value[:4] != '+996':
             raise serializers.ValidationError('Phone number should start with +996 ')
         elif len(value) != 13:
             raise serializers.ValidationError("Phone number must be 13 characters long")
-        if not value[1:].isnumeric():
-            raise serializers.ValidationError('Phone must be numeric symbols')
         return value
