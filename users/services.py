a24ee80afd0eee11829b676f4a198e7f0ce93fbb
estebesov01 <sumseasonl1t1@gmail.com>
2022-07-27 18:37:42 +0600

    fix validate for phone and optimize code

--- users/services.py
+++ users/services.py
@@ -6,14 +6,16 @@ from rest_framework import serializers
 class ModelSerializerWithValidate(serializers.ModelSerializer):
     age = serializers.SerializerMethodField()
 
-    def validate(self, data):
-        if data['phone'][:4] != '+996':
-            raise serializers.ValidationError('Phone number should start with +996 ')
-        elif len(data['phone']) != 13:
-            raise serializers.ValidationError("Phone number must be 13 characters long")
-        return data
-
     def get_age(self, obj):
         today = date.today()
         return today.year - obj.birth_date.year - (
                 (today.month, today.day) < (obj.birth_date.month, obj.birth_date.day))
+
+    def validate_phone(self, value):
+        if value[:4] != '+996':
+            raise serializers.ValidationError('Phone number should start with +996 ')
+        elif len(value) != 13:
+            raise serializers.ValidationError("Phone number must be 13 characters long")
+        if not value[1:].isnumeric():
+            raise serializers.ValidationError('Phone must be numeric symbols')
+        return value
