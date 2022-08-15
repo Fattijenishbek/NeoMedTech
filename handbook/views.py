34694cb1173f58b349a5c4da3d147cfb6183073c
estebesov01 <sumseasonl1t1@gmail.com>
2022-08-16 01:39:29 +0600

    fix little bugs

--- handbook/views.py
+++ handbook/views.py
@@ -19,16 +19,16 @@ from users.permissions import (
 class HandBookViewSet(viewsets.ModelViewSet):
     serializer_class = HandBookSerializer
     queryset = Handbook.objects.all()
-    permission_classes = (IsAuthenticatedOrReadOnly,
-                          IsSuperUserOrOfficeManager,)
+    # permission_classes = (IsAuthenticatedOrReadOnly,
+    #                       IsSuperUserOrOfficeManager,)
     lookup_field = 'week'
 
 
 class TodoViewSet(viewsets.ModelViewSet):
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
-    permission_classes = (IsAuthenticatedOrReadOnly,
-                          IsPatientOrOfficeManager,)
+    # permission_classes = (IsAuthenticatedOrReadOnly,
+    #                       IsPatientOrOfficeManager,)
 
     def list(self, request, *args, **kwargs):
         serializer = TodoListSerializer(Todo.objects.all(), many=True)
@@ -38,20 +38,20 @@ class TodoViewSet(viewsets.ModelViewSet):
 class EssentialsViewSet(viewsets.ModelViewSet):
     serializer_class = EssentialsSerializer
     queryset = Essentials.objects.all()
-    permission_classes = (IsAuthenticatedOrReadOnly,
-                          IsPatientOrOfficeManager,)
+    # permission_classes = (IsAuthenticatedOrReadOnly,
+    #                       IsPatientOrOfficeManager,)
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
     permission_classes = (IsSuperUserOrOfficeManager,)
     queryset = Article.objects.all()
-    permission_classes = (IsAuthenticatedOrReadOnly,
-                          IsSuperUserOrOfficeManager,)
+    # permission_classes = (IsAuthenticatedOrReadOnly,
+    #                       IsSuperUserOrOfficeManager,)
 
 
 class FAQViewSet(viewsets.ModelViewSet):
     serializer_class = FAQSerializer
     queryset = FAQ.objects.all()
-    permission_classes = (IsAuthenticatedOrReadOnly,
-                          IsSuperUserOrOfficeManager,)
+    # permission_classes = (IsAuthenticatedOrReadOnly,
+    #                       IsSuperUserOrOfficeManager,)
